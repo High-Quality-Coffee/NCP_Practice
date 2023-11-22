ba197b379b93dfc7a715389d7063dbc5c1590a7c
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-11-22 17:31:12 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React whats up man
+          Learn React whats up
         </a>
       </header>
     </div>
